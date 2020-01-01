c100d715b155b91020e1034c80124bd886ba0e98
Michael Kolber <michaelkolber@users.noreply.github.com>
2019-12-31 21:30:24 -0500

    Clean up namespaces and unnecessary variables

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@
 
 import express = require('express');
 import dbConnection = require('./dbConnection');  // The connection lives here and is `require`ed whenever needed. Relies on Node's module caching.
-import { createErrorMessage } from './helpers';
+import helpers = require('./helpers');
 
 const app = express();
 const port = process.env.PORT || 1900;
@@ -18,21 +18,16 @@ app.use(express.json());
 
 // Set up routes
 const apiRouter = express.Router(); // Tie everything to this router so that we can run everything through '/api'
+apiRouter.use('/classes', require('./routes/classes'));
+apiRouter.use('/professors', require('./routes/professors'));
+apiRouter.use('/search', require('./routes/search'));
+
 app.use('/api', apiRouter);
-
-import classesRouter = require('./routes/classes');
-apiRouter.use('/classes', classesRouter);
-
-import professorsRouter = require('./routes/professors');
-apiRouter.use('/professors', professorsRouter);
-
-import searchRouter = require('./routes/search');
-apiRouter.use('/search', searchRouter);
 
 
 // Catch-all. Also serves as an easy way to make sure the API is up.
 app.all('/', (req, res) => {
-    const message = createErrorMessage("All requests must go through '/api'.");
+    const message = helpers.createErrorMessage("All requests must go through '/api'.");
     
     res.status(403).json(message);
 });
