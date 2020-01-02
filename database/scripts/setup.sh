78465c72542e7b8cd844bd034c95707a894d3c69
Michael Kolber <michaelkolber@users.noreply.github.com>
2020-01-02 17:34:58 -0500

    FIx some typos

--- database/scripts/setup.sh
+++ database/scripts/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Set up the user and the database in 2 steps
-psql -U postgres -a -f /usr/src/work/init.sql && psql -U postgres -d qc -a -f /usr/src/work/setup.sql;
+psql -U postgres -a -f /usr/src/work/scripts/init.sql && psql -U postgres -d qc -a -f /usr/src/work/scripts/setup.sql;
